76d70dc224969694e26714f9665567226bf27387
Denis Veselov <veselov@trilan.ru>
2013-06-27 15:34:54 +0400

    add tests for empty location in cookies

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -57,6 +57,16 @@ class LocationCookieStorageTest(TestCase):
         storage = LocationCookieStorage(request=self.request, response=HttpResponse())
         self.assertTrue(storage._should_update_cookie(new_value=10))
 
+    def test_should_update_cookie_if_cookies_is_empty_value(self):
+        value = settings.GEOIP_LOCATION_EMPTY_VALUE
+        storage = LocationCookieStorage(request=self.request, response=HttpResponse())
+        self.assertTrue(storage._should_update_cookie(new_value=value))
+
+    def test_validate_location_if_cookies_is_empty_value(self):
+        value = settings.GEOIP_LOCATION_EMPTY_VALUE
+        storage = LocationCookieStorage(request=self.request, response=HttpResponse())
+        self.assertTrue(storage._validate_location(location=value))
+
     @patch.object(settings, 'GEOIP_LOCATION_MODEL', 'test_app.models.MyCustomLocation')
     def test_malicious_cookie_is_no_problem(self):
         self.request.COOKIES[settings.GEOIP_COOKIE_NAME] = "wtf"
