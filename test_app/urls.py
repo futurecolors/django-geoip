26ade322a5263e2b03e6e5d22961e6c6be600167
Semen Pupkov <semen.pupkov@gmail.com>
2014-02-04 22:54:34 +0600

    fix(urls): fix set urls form django_geoip urls.py

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 try:
-    from django.conf.urls import patterns
+    from django.conf.urls import patterns, include
 except ImportError:
-    from django.conf.urls.defaults import patterns
+    from django.conf.urls.defaults import patterns, include
 from django.http import HttpResponse
 from django_geoip.views import set_location
 
@@ -13,6 +13,6 @@ def index_view(request):
 
 urlpatterns = patterns('',
     ('^$', index_view),
-    ('^set_location/$', set_location),
+    (r'^geoip/', include('django_geoip.urls')),
     ('^hello/$', index_view),
 )
