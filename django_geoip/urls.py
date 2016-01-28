2c02b348af92fa411b93e1fa3c558fcd394cdec1
blag <blag@users.noreply.github.com>
2016-01-28 02:04:38 -0700

    Make urlpatterns a normal Python list

--- django_geoip/urls.py
+++ django_geoip/urls.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 try:
-    from django.conf.urls import *
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import *
+    from django.conf.urls.defaults import url
 
 from django_geoip.views import set_location
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^setlocation/', set_location, name='geoip_change_location'),
-)
+]
